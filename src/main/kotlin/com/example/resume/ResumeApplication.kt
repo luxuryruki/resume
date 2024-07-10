02fd9adecb145ff2e292a2f2f6dd1b420c2e1c33
KANG JIHYO <luxuryjihyo@naver.com>
2024-07-10 17:52:13 +0900

    first commit

---
+++ src/main/kotlin/com/example/resume/ResumeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.resume
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ResumeApplication
+
+fun main(args: Array<String>) {
+    runApplication<ResumeApplication>(*args)
+}
